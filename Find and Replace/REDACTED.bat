eff414cfb0058b63610dac078c4f0dc7408c1d01
ttrncic <tomica.trncic@gmail.com>
2019-02-18 10:45:33 +0100

    mod,ref[FAR]: add Bat_To_Exe_Converter, reword REDACTED statements

--- Find and Replace/REDACTED.bat
+++ Find and Replace/REDACTED.bat
@@ -1,7 +1,7 @@
-@RD /S /Q "REDACTED"
-DEL REDACTED DIR
+; @RD /S /Q "REDACTED directory location"
+; DEL REDACTED file location
 
-far --remove  "REDACTED" "\"rc\": 1,"
-far --remove  "REDACTED" "\"rc\": 2,"
+far --remove  "REDACTED config file location" "\"rc\": 1,"
+far --remove  "REDACTED config file location" "\"rc\": 2,"
 
-start "" "REDACTED.exe"
+start "" "REDACTED.exe file location"
