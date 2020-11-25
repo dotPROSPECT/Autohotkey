80c1c03e06d74cb54e5f1d8fb0df87cb3d28ea8b
Thom <tomica.trncic@gmail.com>
2020-11-25 08:56:42 +0100

    ref: update user_aliases

--- cmder/user_aliases.cmd
+++ cmder/user_aliases.cmd
@@ -5,23 +5,29 @@
 ;= goto:eof
 ;= Add aliases below here
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
 history=cat "%CMDER_ROOT%\config\.history"
 unalias=alias /d $1
 cmderr=cd /d "%CMDER_ROOT%"
-;=
+
 ;= Custom
+cpt=cd $b clip
+
+;= Npm commands
+dev=npm run dev
+adev=npm run local
+
+;= Git commands
+gl=git log --oneline --all --graph --decorate  $*
 go=git remote update $t git branch -vv
 gp=git pull
 gs=git status
 ga=git add --all
 gg=git commit
 gc=git checkout -- .
-dev=npm run dev
-~1=~0,-1  
-cpt=cd $b clip  
-tgcl=git clone git@thorne:$*  
-tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne  
-ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica  
-ngcl=git clone git@neo:$*  
+
+;= User specific git commands
+tgcl=git clone git@thorne:$*
+tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne
+ngcl=git clone git@neo:$*
+ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica
