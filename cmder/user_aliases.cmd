71a92a9cfc57dd5e9b87bbfd92f151da201bb717
Thom <tomica.trncic@gmail.com>
2020-12-02 21:01:08 +0100

    unit[cmder]: add new aliases

--- cmder/user_aliases.cmd
+++ cmder/user_aliases.cmd
@@ -5,23 +5,31 @@
 ;= goto:eof
 ;= Add aliases below here
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
-ls=ls --show-control-chars -F --color $*
 history=cat "%CMDER_ROOT%\config\.history"
+
+;= CMDER
 unalias=alias /d $1
-cmderr=cd /d "%CMDER_ROOT%"
-;=
-;= Custom
+
+;= GIT
+gl=git log --oneline --all --graph --decorate  $*
 go=git remote update $t git branch -vv
 gp=git pull
 gs=git status
 ga=git add --all
 gg=git commit
 gc=git checkout -- .
+tgcl=git clone git@thorne:$*
+tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne
+ngcl=git clone git@neo:$*
+ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica
+
+;= NPM
 dev=npm run dev
-~1=~0,-1  
-cpt=cd $b clip  
-tgcl=git clone git@thorne:$*  
-tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne  
-ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica  
-ngcl=git clone git@neo:$*  
+npmug=npm update -g
+npmls=npm list -g --depth 0
+
+;= NAVIGATION
+ls=ls --show-control-chars -F --color $*
+cmderr=cd /d "%CMDER_ROOT%"
+cpt=cd $b clip
+utilsdir=cd C:\Users\Thorne\Geekzone\Utilities
