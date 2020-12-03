930dc1f5ca707abe8318a536c3a7be1b6bfd8237
Thom <tomica.trncic@gmail.com>
2020-12-03 10:19:11 +0100

    unit[cmder]: update aliases

--- cmder/user_aliases.cmd
+++ cmder/user_aliases.cmd
@@ -19,10 +19,14 @@ ga=git add --all
 gg=git commit
 gc=git checkout -- .
 gcc=git branch --contains $*
+gcd=git checkout development
+gcq=git checkout qa
+gcp=git cherry-pick $*
 tgcl=git clone git@thorne:$*
 tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne
 ngcl=git clone git@neo:$*
 ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica
+
 
 ;= NPM
 dev=npm run dev
@@ -34,3 +38,4 @@ ls=ls --show-control-chars -F --color $*
 cmderr=cd /d "%CMDER_ROOT%"
 cpt=cd $b clip
 utilsdir=cd C:\Users\Thorne\Geekzone\Utilities
+
