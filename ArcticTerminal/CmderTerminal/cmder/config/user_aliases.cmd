6601fe76b60d78137b6cd2b4cc7b63a1b7ff45a0
Thom <tomica.trncic@gmail.com>
2021-03-06 15:56:55 +0100

    unit[Arctic]: centralize cmder terminal

--- ArcticTerminal/CmderTerminal/cmder/config/user_aliases.cmd
+++ ArcticTerminal/CmderTerminal/cmder/config/user_aliases.cmd
@@ -26,8 +26,10 @@ gcq=git checkout qa
 gcp=git cherry-pick $*
 tgcl=git clone git@thorne:$*
 tgit=git config user.email tomica.trncic@gmail.com $t git config user.name Thom Thorne
-ngcl=git clone git@neo:$*
-ngit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica
+nsgcl=git clone git@ns:$*
+nsgit=git config user.email tomica@neostar.com $t git config user.name tomica
+azgcl=git clone git@az:$*
+azgit=git config user.email ttrncic@autozubak.hr $t git config user.name tomica
 
 
 ;= NPM
